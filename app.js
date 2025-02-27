9b78b17d22d4774d34962cffc96259ae3b270d1d
Apalchikov <allfatherdmd@mail.ru>
2025-02-27 22:04:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-// Данные о товарах (можно заменить на загрузку с сервера)
+// Данные о товарах
 const products = [
   {
     id: 1,
-    name: "Футболка",
+    name: "Футболка Dynasty",
     price: 1500,
-    image: "https://via.placeholder.com/150",
-    description: "Мягкая и удобная футболка."
+    image: "https://static.insales-cdn.com/r/JBn0FfTLI1A/rs:fit:1000:0:1/q:100/plain/images/products/1/5852/655922908/%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0_%D1%87%D0%B5%D1%80%D0%BD%D0%B0%D1%8F_1.jpg@jpg", // Замени на свои фото
+    description: "Черная футболка с логотипом."
   },
   {
     id: 2,
-    name: "Джинсы",
+    name: "Худи Dynasty",
     price: 3000,
-    image: "https://via.placeholder.com/150",
-    description: "Стильные джинсы."
+    image: "https://static.insales-cdn.com/r/XKMchTf_wnY/rs:fit:1000:0:1/q:100/plain/images/products/1/3574/814149110/%D0%A5%D1%83%D0%B4%D0%B8_%D1%87%D0%B5%D1%80%D0%BD_%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F.jpg@jpg", // Замени на свои фото
+    description: "Удобное худи с капюшоном."
   },
   {
     id: 3,
-    name: "Куртка",
+    name: "Куртка Dynasty",
     price: 5000,
-    image: "https://via.placeholder.com/150",
-    description: "Теплая куртка."
+    image: "https://static.shuclothes.com/sig/size:8192/aHR0cHM6Ly9zaHVjbG90aGVzLmNvbS9zdG9yYWdlLzIzNzk3L9C60L7RgNC-0YLQutCw0Y9f0LbQtdC90YHQul_Rh9C10YDQvTA2NDMuanBn", // Замени на свои фото
+    description: "Стильная куртка для улицы."
+  },
+  {
+    id: 4,
+    name: "Штаны Dynasty",
+    price: 2500,
+    image: "https://nikifilinistore.ru/wp-content/uploads/2023/08/105.jpeg", // Замени на свои фото
+    description: "Удобные штаны для повседневной носки."
   }
 ];
 
